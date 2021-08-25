a851f583ef68018dbe0634e240fb83838906cf6f
Mane <mane_kostanyan@epam.com>
2021-08-25 10:52:49 +0400

    Added Report portal

--- src/main/java/utils/TestListener.java
+++ src/main/java/utils/TestListener.java
@@ -1,5 +1,7 @@
 package utils;
 
+import com.epam.reportportal.message.ReportPortalMessage;
+import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import driver.DriverSingleton;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
@@ -15,30 +17,53 @@ import java.time.format.DateTimeFormatter;
 
 public class TestListener implements ITestListener {
 
-    protected Logger log = LogManager.getRootLogger();
+    public Logger log = LogManager.getRootLogger();
 
     @Override
     public void onTestFailure(ITestResult result) {
         saveScreenshot();
     }
 
-    private void saveScreenshot() {
-        File screenCapture = ((TakesScreenshot) DriverSingleton
-                .getDriver())
-                .getScreenshotAs(OutputType.FILE);
+//    private void saveScreenshot() {
+//        File screenCapture = ((TakesScreenshot) DriverSingleton
+//                .getDriver())
+//                .getScreenshotAs(OutputType.FILE);
+//        try {
+//            FileUtils.copyFile(screenCapture, new File(
+//                    ".//target/screenshots/"
+//                            + getCurrentTimeAsString() +
+//                            ".png"));
+//        } catch (IOException e) {
+//            log.error("Failed to save screenshot: " + e.getLocalizedMessage());
+//        }
+//    }
+
+    private void saveScreenshot(){
+        ReportPortalMessage message = null;
         try {
-            FileUtils.copyFile(screenCapture, new File(
-                    ".//target/screenshots/"
-                            + getCurrentTimeAsString() +
-                            ".png"));
+            TakesScreenshot ts = (TakesScreenshot) DriverSingleton.getDriver();
+            File srcFile = ts.getScreenshotAs(OutputType.FILE);
+            java.util.Date d= new java.util.Date();
+            org.apache.commons.io.FileUtils.copyFile(srcFile, new File("./ScreenShots/"+d.toString().replace(":", "_")+".png"));
+            String rp_message = "test message for ReportPortal" + getCurrentTimeAsString();
+            message = new ReportPortalMessage(srcFile, rp_message);
         } catch (IOException e) {
-            log.error("Failed to save screenshot: " + e.getLocalizedMessage());
+            e.printStackTrace();
         }
+        log.info(message);
+
     }
+
 
     private String getCurrentTimeAsString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd_HH-mm-ss");
         return ZonedDateTime.now().format(formatter);
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+            log.info(result.getName());
+
     }
 }
 
